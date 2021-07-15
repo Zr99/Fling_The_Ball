e335d508b99056b53ed4776f5e14a9b81710993a
Zr99 <38181091+Zr99@users.noreply.github.com>
2021-07-16 04:21:48 +1200

    Update MainActivity.java

--- app/src/main/java/com/example/assignmentseven/MainActivity.java
+++ app/src/main/java/com/example/assignmentseven/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.assignmentseven;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -13,12 +12,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-     public EditText mEdit;
+    public EditText mEdit;
     public TextView mText;
     private String input = "";
     public static ArrayList<String> nameList = new ArrayList<>();
@@ -29,9 +27,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
-
-        //TextView tw = (TextView)findViewById(R.id.username);
-
         //Full-Screen
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
@@ -52,18 +47,14 @@ public class MainActivity extends AppCompatActivity {
         else{
 
             Toast.makeText(this, "Game start", Toast.LENGTH_SHORT).show();
-
             Intent i = new Intent(this, GameScreen.class);
-
             nameList.add(input);
             //i.setAction(i.ACTION_SEND);
             //i.setType("text/plain");
             i.putExtra("username",input);
-
             startActivity(i);
             mEdit.setText(null);
         }
-
     }
 
     public void onClickHigh(View v){
@@ -73,7 +64,4 @@ public class MainActivity extends AppCompatActivity {
         i.putExtra("scoreArray",scoreList);
         startActivity(i);
     }
-
-
-
 }
